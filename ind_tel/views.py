8faa7ba164f82197b641a49750cfad6b408f5b82
Shaik Afreen <sameenaghouse77@gmail.com>
2026-05-06 09:55:14 +0530

    Initial commit

--- ind_tel/views.py
+++ ind_tel/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hyderabad(request):
+    places=['golkonda','sanghi','charminar','chilkur','ramoji film city']
+    context={"places":places}
+    
     return render(request,'hyderabad.html')
 def charminar(request):
     response="<h1>Welcome to Charminar</h1>"
